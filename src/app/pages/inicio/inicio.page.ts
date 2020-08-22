ae0ce1cdf8d26176ed7549d30f34ef6e2b4f510f
Dario Zubaray <dzubaray@gmail.com>
2020-08-22 20:04:33 -0300

    implementando el menu lateral, moviendo la interfaz Componente. Extrayendo las entrada de menu desde un archivo json en los assets

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Componente } from '../../interfaces/Componente';
+import { DataService } from '../../services/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-inicio',
@@ -7,88 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  componentes: Componente[] = [
-    {
-      icon: 'american-football-outline',
-      name: 'Action Sheet',
-      redirecTo: '/action-sheet'
-    },
-    {
-      icon: 'logo-google-playstore',
-      name: 'Alert',
-      redirecTo: '/alert'
-    },
-    {
-      icon: 'beaker-outline',
-      name: 'Avatar',
-      redirecTo: '/avatar'
-    },
-    {
-      icon: 'radio-button-on-outline',
-      name: 'Botones',
-      redirecTo: '/botones'
-    },
-    {
-      icon: 'card-outline',
-      name: 'Card',
-      redirecTo: '/card'
-    },
-    {
-      icon: 'checkmark-circle-outline',
-      name: 'Check',
-      redirecTo: '/check'
-    },
-    {
-      icon: 'calendar-outline',
-      name: 'Date Time',
-      redirecTo: '/date-time'
-    },
-    {
-      icon: 'car-outline',
-      name: 'Floating Action Button',
-      redirecTo: '/fab'
-    },
-    {
-      icon: 'grid-outline',
-      name: 'Grid',
-      redirecTo: '/grid'
-    },
-    {
-      icon: 'infinite-outline',
-      name: 'Infinite Scroll',
-      redirecTo: '/infinite-scroll'
-    },
-    {
-      icon: 'hammer-outline',
-      name: 'Inputs Form',
-      redirecTo: '/input'
-    },
-    {
-      icon: 'list-outline',
-      name: 'Lista Slidding',
-      redirecTo: '/lista'
-    },
-    {
-      icon: 'reorder-four-outline',
-      name: 'Lista Reorder',
-      redirecTo: '/list-reorder'
-    },
-    {
-      icon: 'refresh-circle-outline',
-      name: 'Loading',
-      redirecTo: '/loading'
-    }
-  ];
+  componentes: Observable<Componente[]>;
 
-  constructor() { }
+  constructor(private menuController: MenuController,
+              private dataService: DataService) { }
 
   ngOnInit() {
+    this.componentes = this.dataService.getMenuOpt()
   }
 
-}
-
-interface Componente {
-  icon: string;
-  name: string;
-  redirecTo: string;
+  toggleMenu() {
+    this.menuController.toggle();
+  }
 }
