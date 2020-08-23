3aa870ebda697d6b9e83872567fced5f5617fea6
Dario Zubaray <dzubaray@gmail.com>
2020-08-23 16:24:14 -0300

    Se agrega un mensjae toast en las listas sliding

--- src/app/pages/lista/lista.page.ts
+++ src/app/pages/lista/lista.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Observable } from 'rxjs';
-import { IonList } from '@ionic/angular';
+import { IonList, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista',
@@ -13,24 +13,36 @@ export class ListaPage implements OnInit {
   @ViewChild('lista') lista: IonList;
   users: Observable<any>;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService,
+              private toastController: ToastController) { }
 
   ngOnInit() {
     this.users = this.dataService.getUsers();
   }
 
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
   favorite(user) {
     console.log('favorite', user)
+    this.presentToast('Se guardó en favoritos')
     this.lista.closeSlidingItems()
   }
 
   share(user) {
     console.log('share', user)
+    this.presentToast('Se ha compartido')
     this.lista.closeSlidingItems()
   }
 
   unread(user) {
     console.log('unread', user)
+    this.presentToast('Se marcó como borrado')
     this.lista.closeSlidingItems()
   }
 }
